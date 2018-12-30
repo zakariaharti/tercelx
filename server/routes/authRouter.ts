82ed9705534400cb1d086b869fdb31b030669407
zakariaharti <zakariya.developer@gmail.com>
2018-12-30 20:02:17 +0100

    Update authRouter.ts

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,10 +1,26 @@
 import * as express from 'express';
 import { login, signup } from '../controllers/authController';
+import { body } from 'express-validator/check';
 import * as passportConfig from '../../config/passport';
 
 const app = express.Router();
 
-app.post('/signup', signup);
-app.post('/login', passportConfig.requireLogin, login);
+app.post('/signup',[
+  body('email')
+   .not().isEmpty()
+   .isEmail()
+   .normalizeEmail(),
+  body('name')
+   .not().isEmpty(),
+  body('password')
+   .not().isEmpty()
+   .matches(/.{4,}/)
+], signup);
+app.post('/login',[
+  body('email')
+   .not().isEmpty(),
+  body('password')
+   .not().isEmpty()
+] ,passportConfig.requireLogin, login);
 
 export default app;
