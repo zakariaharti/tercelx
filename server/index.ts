5cf52c46f74a335a622aa73843cf2ec1bcd8dc27
zakariaharti <zakariya.developer@gmail.com>
2019-01-01 19:46:42 +0100

    sd 2

--- server/index.ts
+++ server/index.ts
@@ -32,6 +32,33 @@ const MongoStore = require('connect-mongo')(session);
   */
 const app = express();
 
+// Set Development modes checks
+const isDevMode = process.env.NODE_ENV === 'development' || false;
+const isProdMode = process.env.NODE_ENV === 'production' || false;
+
+// Run Webpack dev server in development mode
+if (isDevMode) {
+  // Webpack Requirements
+  // eslint-disable-next-line global-require
+  const webpack = require('webpack');
+  // eslint-disable-next-line global-require
+  const config = require('../webpack.client.dev');
+  // eslint-disable-next-line global-require
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  // eslint-disable-next-line global-require
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    watchOptions: {
+      poll: 1000,
+    },
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+
 /**
  * connect to MongoDB
  */
@@ -86,7 +113,7 @@ app.use('/api', authRouter);
 /**
  * Error Handler.
  */
-if (process.env.NODE_ENV === 'development') {
+if (!isProdMode) {
   // only use in development
   app.use(errorHandler());
 } else {
