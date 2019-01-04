a98c2834270d8324488a1e3a04023378e3430957
zakariaharti <zakariya.developer@gmail.com>
2019-01-04 21:35:08 +0100

    added scripts

--- server/index.tsx
+++ server/index.tsx
@@ -40,15 +40,12 @@ const isProdMode = process.env.NODE_ENV === 'production' || false;
 // Run Webpack dev server in development mode
 if (isDevMode) {
   // Webpack Requirements
-  // eslint-disable-next-line global-require
   const webpack = require('webpack');
-  // eslint-disable-next-line global-require
   const config = require('../webpack.client.dev');
-  // eslint-disable-next-line global-require
   const webpackDevMiddleware = require('webpack-dev-middleware');
-  // eslint-disable-next-line global-require
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(config);
+
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath,
