76577b10cca87a25d9719789586995ed940f59cd
zakariaharti <zakariya.developer@gmail.com>
2019-01-05 22:40:04 +0100

    App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,2 +1,32 @@
 import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+import { createBrowserHistory } from 'history';
+import { hot } from "react-hot-loader";
+
+import routes from './routes';
+
+const isProdMode = process.env.NODE_ENV === 'production';
+
+interface IApp {
+  store: Store;
+}
+
+const App: React.SFC<IApp> = (props) => (
+  <Provider store={props.store}>
+    <ConnectedRouter history={createBrowserHistory()}>
+      <Router>
+        {routes}
+      </Router>
+    </ConnectedRouter>
+  </Provider>
+);
+
+/**
+ * use hot reloading in development
+ */
+export default (
+  isProdMode ? App : hot(App)(module)
+);
