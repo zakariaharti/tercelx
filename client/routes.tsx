58c580315bcb26e9bd0c8c5559fc7cf1af4a1a6b
zakariaharti <zakariya.developer@gmail.com>
2019-01-05 21:46:49 +0100

    routes

---
+++ client/routes.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Route } from 'react-router-dom';
+
+import App from './modules/App/App';
+
+export default (
+  <Route exact path="/" component={App} />
+);
