890f1f0220d96d893f0aa7ed3d36e9522d771942
zakariaharti <zakariya.developer@gmail.com>
2019-01-05 19:58:57 +0100

    App.scss

---
+++ client/modules/App/Components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Footer: React.SFC<{}> = () => (
+  <div>this is Footer placeholder</div>
+);
+
+export default Footer;
---
+++ client/modules/App/Components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Header: React.SFC<{}> = () => (
+  <div>this is header placeholder</div>
+);
+
+export default Header;
