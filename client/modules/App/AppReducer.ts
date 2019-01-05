5da8d547805f392f7e39aaea09a56512b5e15ae9
zakariaharti <zakariya.developer@gmail.com>
2019-01-05 21:38:00 +0100

    AppReducer

---
+++ client/modules/App/AppReducer.ts
@@ -0,0 +1,12 @@
+const initState = {
+  message: 'hello world'
+};
+
+const AppReducer = (state: any = initState, action: any) => {
+  switch(action.type){
+    default:
+     return state;
+  }
+};
+
+export default AppReducer;
