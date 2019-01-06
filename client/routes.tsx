425e0adf0dfdb284741268d282ab60547370b188
zakariaharti <zakariya.developer@gmail.com>
2019-01-06 20:42:54 +0100

    fetchComponentDataBeforeRender 2

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { RouteConfig } from 'react-router-config';
 
 import App from './modules/App/App';
 
-export default (
-  <Route exact path="/" component={App} />
-);
+const routes: RouteConfig[] = [
+  {
+    component: App
+  }
+]
--- server/lib/helpers/fetchComponentsDataBeforeRender.ts
+++ server/lib/helpers/fetchComponentsDataBeforeRender.ts
@@ -1,19 +1,19 @@
-import { Dispatch } from 'redux';
+import {
+  matchRoutes,
+  RouteConfig
+} from 'react-router-config';
+
 /**
 * This looks at static needs parameter in components and waits for the promise to be fullfilled
 * It is used to make sure server side rendered pages wait for APIs to resolve before returning res.end()
 */
 
-export function fetchComponentDataBeforeRender(
-  dispatch: Dispatch,
-  components: any,
-  params: any
-) {
-  const needs = components.reduce( (prev: any, current: any) => {
-    return (current.need || [])
-      .concat((current.WrappedComponent ? current.WrappedComponent.need : []) || [])
-      .concat(prev);
-    }, []);
-    const promises = needs.map((need: any) => dispatch(need(params)));
-    return Promise.all(promises);
+export function fetchComponentDataBeforeRender(routes: RouteConfig[],pathname: string) {
+  const matches = matchRoutes(routes, pathname);
+  const promises = matches.map(({ route, match }) => {
+    // @ts-ignore
+    return route.loadDate ? route.loadDate(match) : Promise.resolve(null);
+  });
+
+  return Promise.all(promises);
 }
